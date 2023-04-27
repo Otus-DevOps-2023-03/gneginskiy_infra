b0a6b440ddd1cf74676612039b48b8a28d8adf85
gneginskiy <gneginskiy@mail.ru>
2023-04-27 23:17:36 +0400

    fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,11 +4,21 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
-sudo systemctl enable mongod
-sudo systemctl start mongod
+sudo service mongod start
+sudo service mongodb start
 
-sudo systemctl enable mongodb
-sudo systemctl start mongodb
+#7 errors:
+#!/bin/sh
+#sudo apt-get install -y apt-transport-https ca-certificates
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#sudo apt-get update
+#sudo apt-get install -y mongodb-org
+#sudo systemctl enable mongod
+#sudo service mongod start
+#
+#sudo systemctl enable mongodb
+#sudo systemctl start mongodb
 
 # 8 errors:...
 #
