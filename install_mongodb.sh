78e2491abd6626b7ea9ca3bb71d3dd329ee13210
gneginskiy <gneginskiy@mail.ru>
2023-04-27 22:58:50 +0400

    fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,7 +2,8 @@
 sudo apt-get install -y apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-sudo apt-get update
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt update
 sudo apt-get install -y mongodb-org
 sudo apt-get install -y mongodb
 
