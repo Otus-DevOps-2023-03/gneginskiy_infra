8425b8ce57e24819c000c1e13c900d5bb73d6e5e
gneginskiy <gneginskiy@mail.ru>
2023-04-27 19:55:18 +0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 pkill -f puma
-sudo apt-get install git
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
+sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit
 bundle install
